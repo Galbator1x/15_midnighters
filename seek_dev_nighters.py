084d95f86c66733cdeec0ff1505bc7b59fbaafee
Ivan Filimonov <snake73w@gmail.com>
2016-10-10 03:23:35 +0700

    Added script.

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,40 @@
+from datetime import datetime
+
+from pytz import timezone
+import requests
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    url = 'https://devman.org/api/challenges/solution_attempts/'
+    payload = {'page': '1'}
+    req = requests.get(url, params=payload).json()
+    pages = req['number_of_pages']
+
+    for page in range(1, pages + 1):
+        payload = {'page': page}
+        req = requests.get(url, params=payload).json()
+        for record in req['records']:
+            if record['timestamp'] is None:
+                continue
+            yield {
+                'username': record['username'],
+                'timestamp': record['timestamp'],
+                'timezone': record['timezone'],
+            }
+
 
 def get_midnighters():
-    pass
+    midnighters = set()
+    for record in load_attempts():
+        tz = timezone(record['timezone'])
+        dt = tz.localize(datetime.utcfromtimestamp(record['timestamp']))
+        dt_00 = tz.localize(datetime(dt.year, dt.month, dt.day, 0, 0, 0))
+        dt_06 = tz.localize(datetime(dt.year, dt.month, dt.day, 6, 0, 0))
+        if dt_00 < dt < dt_06:
+            midnighters.add(record['username'])
+    return midnighters
+
 
 if __name__ == '__main__':
-  pass
+    print('Midnighters:')
+    [print(name) for name in get_midnighters()]
